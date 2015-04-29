4909c8ec9dc7952a964a9683f30387fc3d3737b0
bitsofinfo <bitsofinfo.g@gmail.com>
2015-04-29 09:15:23 -0600

    latest

--- src/main/scala/org/bitsofinfo/es/snapmgr/SnapshotManager.scala
+++ src/main/scala/org/bitsofinfo/es/snapmgr/SnapshotManager.scala
@@ -31,6 +31,51 @@ import scala.io.StdIn
 import scala.concurrent.duration._
 
 
+// [repositoryRoot]/index
+//      {"snapshots":["snap99","snap102","vectorprime","snap1","snap55","snap56","snap57","ls1","snap100","snap101"]}
+
+// [repositoryRoot]/snapshot-[snapshotName]
+//     {"snapshot":{"name":"ls1","version_id":1050299,"indices":["logstash-2015.02.24"],"state":"SUCCESS","start_time":1430316763665,"end_time":1430316862727,"total_shards":5,"successful_shards":5,"failures":[]}}
+
+// [repositoryRoot]/metadata-[snapshotName]
+//     {"meta-data":{"version":74,"uuid":"Q7Oabmf-Tz-wVIix-hTEkg","templates":{}}}
+
+// [repositoryRoot]/indices/[indexName]/snapshot-[snapshotName]
+//     JSON file contains all index settings/config etc
+
+// [repositoryRoot]/indices/[indexName]/[shard#]/snapshot-[snapshotName]
+
+/*     Contains list of all segment files within the shard directory
+       that are part of the snapshot
+    {
+      "name" : "ls1",
+      "index_version" : 83,
+      "start_time" : 1430316763701,
+      "time" : 97221,
+      "number_of_files" : 289,
+      "total_size" : 2052566131,
+      "files" : [ {
+        "name" : "__0",
+        "physical_name" : "_mlm_Lucene410_0.dvd",
+        "length" : 404,
+        "checksum" : "1fyhsxd",
+        "written_by" : "4.10.3"
+      }, {
+        "name" : "__1",
+        "physical_name" : "_mqc_Lucene410_0.dvm",
+        "length" : 91,
+        "checksum" : "a9bz95",
+        "written_by" : "4.10.3"
+      }, {
+        "name" : "__2",
+        "physical_name" : "_m62.si",
+        "length" : 455,
+        "checksum" : "14b1wg0",
+        "written_by" : "4.10.3",
+        "meta_hash" : "P9dsFxNMdWNlbmU0NlNlZ21lbnRJbmZvAAAAAQY0LjEwLjMAAFHn/wAAAAoJdGltZXN0YW1wDTE0MjQ4MjExNTUyMjMLbWVyZ2VGYWN0b3ICMTAKb3MudmVyc2lvbhoyLjYuMzItNDMxLjE3LjEuZWw2Lng4Nl82NAJvcwVMaW51eA5sdWNlbmUudmVyc2lvbgY0LjEwLjMGc291cmNlBW1lcmdlB29zLmFyY2gFYW1kNjQTbWVyZ2VNYXhOdW1TZWdtZW50cwItMQxqYXZhLnZlcnNpb24IMS43LjBfNTULamF2YS52ZW5kb3IST3JhY2xlIENvcnBvcmF0aW9uAAAADRBfbTYyX2VzMDkwXzAudGlwFF9tNjJfTHVjZW5lNDEwXzAuZHZkB19tNjIuc2kQX202Ml9lczA5MF8wLmRvYxRfbTYyX0x1Y2VuZTQxMF8wLmR2bQhfbTYyLm52bRBfbTYyX2VzMDkwXzAuYmxtCF9tNjIubnZkCF9tNjIuZmR0EF9tNjJfZXMwOTBfMC5wb3MIX202Mi5mZHgQX202Ml9lczA5MF8wLnRpbQhfbTYyLmZubcAok+gAAAAAAAAAAJFE5YA="
+    }, ........
+*/
+
 class SnapshotManager {
 
     val client = ElasticClient.remote("localhost",9300)
@@ -60,9 +105,9 @@ class SnapshotManager {
         nodes.toList
     }
 
-    def getRepositories(client:ElasticClient):List[Repository] = {
+    def getRepositories(client:ElasticClient, repoNames:Seq[String] = Seq("_all")):List[Repository] = {
 
-        val listReposRequest = new GetRepositoriesRequest(Array("_all"))
+        val listReposRequest = new GetRepositoriesRequest(repoNames.toArray)
         val listReposResponse = client.admin.cluster.getRepositories(listReposRequest).get()
 
         val repos = ListBuffer[Repository]()
@@ -72,7 +117,11 @@ class SnapshotManager {
         }
 
         repos.toList
+    }
 
+    def getRepository(client:ElasticClient, repoName:String):Repository = {
+        val foundRepos = getRepositories(client, Seq(repoName))
+        foundRepos.head
     }
 
     def createFSRepository(client:ElasticClient, repoName:String, verify:Boolean, compress:Boolean, location:String):Boolean = {
@@ -114,10 +163,10 @@ class SnapshotManager {
     }
 
 
-    def getSnapshots(client:ElasticClient, repoName:String):List[Snapshot] = {
+    def getSnapshots(client:ElasticClient, repoName:String, snapshotNames:Seq[String] = Seq()):List[Snapshot] = {
 
         val response:GetSnapshotsResponse = client.execute {
-            com.sksamuel.elastic4s.ElasticDsl.get snapshot Seq() from repoName
+            com.sksamuel.elastic4s.ElasticDsl.get snapshot snapshotNames from repoName
             }.await(Duration(30000,MILLISECONDS))
 
         val snapshots = ListBuffer[Snapshot]()
@@ -127,6 +176,11 @@ class SnapshotManager {
         }
 
         snapshots.toList
+    }
+
+    def getSnapshot(client:ElasticClient, repoName:String, snapshotName:String):Snapshot = {
+        val foundSnapshots = getSnapshots(client,repoName,Seq(snapshotName))
+        foundSnapshots.head
     }
 
     def promptForHostConfigProvider():HostConfigProvider = {
@@ -145,6 +199,27 @@ class SnapshotManager {
 
     def getExceptionMessages(e:Throwable, msg:String):String = {
         if (e.getCause != null) getExceptionMessages(e.getCause, (msg +"\n"+e.getMessage)) else (msg+"\n"+e.getMessage)
+    }
+
+
+    def buildSnapshotManifest(client:ElasticClient, repositoryRoot:String, snapshotName:String):List[String] = {
+
+        val manifest = ListBuffer[String]()
+        manifest += repositoryRoot + "/index"
+        manifest += repositoryRoot + "/snapshot-" + snapshotName
+        manifest += repositoryRoot + "/metadata-" + snapshotName
+
+        /*
+        for (shardNum <- (0 to (totalShards-1))) {
+            manifest += repositoryRoot + "/indices/" + indexName + "/" + shardNum + "/snapshot-" + snapshotName
+
+            // pull this file above, parse it from JSON, get the 'files' array, to collect each segment filename
+            // then add each segment filename to the manifest under the same path
+        }
+        */
+
+        manifest.toList
+
     }
 
 }
