d7dfeed82e31a53792199d2684d2b459f245cdd8
bitsofinfo <bitsofinfo.g@gmail.com>
2015-05-05 09:09:18 -0600

    package method + new class

---
+++ src/main/scala/org/bitsofinfo/es/snapmgr/package.scala
@@ -0,0 +1,8 @@
+package org.bitsofinfo.es.snapmgr
+
+package object util {
+        
+    def getExceptionMessages(e:Throwable, msg:String):String = {
+        if (e.getCause != null) getExceptionMessages(e.getCause, (msg +"\n"+e.getMessage)) else (msg+"\n"+e.getMessage)
+    }
+}
