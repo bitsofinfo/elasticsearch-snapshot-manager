a1a47913796bff6cf0b02c7431edb358a10de573
bitsofinfo <bitsofinfo.g@gmail.com>
2015-05-05 17:59:01 -0600

    refactor

--- src/main/scala/org/bitsofinfo/es/snapmgr/ElasticService.scala
+++ src/main/scala/org/bitsofinfo/es/snapmgr/ElasticService.scala
@@ -1,16 +1,18 @@
 package org.bitsofinfo.es.snapmgr
 
+import scala.collection.immutable._
+
 trait ElasticService {
 
     def discoverNodes():List[Node]
     def getPrimaryNodesForIndex(indexName:String):List[Node]
 
-    def getRepositories(repoNames:Seq[String] = Seq("_all")):List[Repository]
+    def getRepositories(repoNames:Seq[String]):List[Repository]
     def getRepository(repoName:String):Repository
     def createFSRepository(repoName:String, verify:Boolean, compress:Boolean, location:String):Boolean
     def deleteRepository(repoName:String):Boolean
 
     def createSnapshot(repoName:String, snapshotName:String, indexName:String):SnapshotResult
-    def getSnapshots(repoName:String, snapshotNames:Seq[String] = Seq()):List[Snapshot]
+    def getSnapshots(repoName:String, snapshotNames:Seq[String]):List[Snapshot]
     def getSnapshot(repoName:String, snapshotName:String):Snapshot
 }
